4d0922620b923958bed1b59d196729cdb8465287
bulldoguk <gary@myhmbiz.com>
2021-11-04 23:21:25 -0500

    Basic port of events page and widget ported over

--- modules/@apostrophecms/event-page/index.js
+++ modules/@apostrophecms/event-page/index.js
@@ -1,3 +1,6 @@
 module.exports = {
-  extend: '@apostrophecms/page-type'
+  extend: '@apostrophecms/piece-page-type',
+  options: {
+    piecesFilters: [{ name: 'year' }, { name: 'month' }, { name: 'day' }]
+  }
 };
--- modules/@apostrophecms/event-widget/index.js
+++ modules/@apostrophecms/event-widget/index.js
@@ -1,7 +1,3 @@
 module.exports = {
-  label: 'Events Widget',
   extend: '@apostrophecms/widget-type',
-
-  byAllLabel: 'Upcoming Events',
-  byTagLabel: 'Upcoming Events by Tag'
 };
